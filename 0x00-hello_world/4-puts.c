8de7a7753fc9f32839950a5b27dca58cf029a0df
DOUNIAELMOUJOUD <anouarmoujoud10@gmail.com>
2023-06-15 14:06:58 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,7 +3,7 @@
 
 /**
  * main - prints "Programming is like building a multilingual
- * puzzle, followed by a new line.
+ * puzzle, followed by a newline.
  * Return: Always 0.
  */
 
@@ -12,6 +12,6 @@ int main(void)
 
 
 { 	
-        puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+         puts("\"Programming is like building a multilingual puzzle");
+         return(0);
 }	
